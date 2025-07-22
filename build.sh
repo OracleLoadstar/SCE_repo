61dc3212415f947b74892a40a1e4802cfb1965c7
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-07-22 22:43:33 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+mv -vf android ..
+mv -vf arch_repo ..
 apt-ftparchive packages . > Packages 
 apt-ftparchive release . > Release 
 gpg --clearsign -o InRelease Release
 gpg -abs -o Release.gpg Release
+vim ./Packages		
+apt-ftparchive release . > Release
+gpg --clearsign -o InRelease Release
+gpg -abs -o Release.gpg Release
+mv -vf ../android .
+mv -vf ../arch_repo .
